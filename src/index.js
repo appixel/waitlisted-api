986d408fd81477b8e8f444eea98ab19cee9ce068
Nathan Charles <nathan@nathancharles.com>
2020-05-21 11:40:47 -0500

    Hello world

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/auth', (req, res) => {
+  res.send('Hello World, from express');
+});
+ 
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
